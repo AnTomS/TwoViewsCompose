e64668b2436a1121c31fd929b1cca1417ad8f610
AnTomS <thekeibee@yandex.ru>
2023-07-10 09:26:22 +0800

    add main

--- app/src/main/java/com/example/twoviewscompose/MainActivity.kt
+++ app/src/main/java/com/example/twoviewscompose/MainActivity.kt
@@ -3,44 +3,42 @@ package com.example.twoviewscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.twoviewscompose.ui.theme.TwoViewsComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TwoViewsComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyApp() {
+    Surface(color = MaterialTheme.colors.background) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val number1 = remember { mutableStateOf(0) }
+            val number2 = remember { mutableStateOf(0) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TwoViewsComposeTheme {
-        Greeting("Android")
+            InputFields(number1, number2)
+            Spacer(modifier = Modifier.height(16.dp))
+            NextButton(number1, number2)
+        }
     }
 }
